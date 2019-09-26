cd9125e70e26ff33a67d7aca4e4cd32547dc031a
Gregoire Hornung <gregoire.hornung@bain.com>
2019-09-26 11:58:07 +0200

    read data from github

--- src/foo.py
+++ src/foo.py
@@ -5,8 +5,11 @@ from sklearn import datasets
 import matplotlib.pyplot as plt
 ##}
 
+#bad, bad, bad practice to store data on github
+data_dir = "https://github.com/greghor/vimux-ds-workflow/tree/master/data/"
 
 ##{
+clients = pd.read_csv(data_dir + "clients.csv")
 iris = datasets.load_iris()
 df = pd.DataFrame(iris.data, columns=iris.feature_names)
 df["target"] = iris.target
